01966c073e887db29c284679e005d25cd164fee9
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-22 15:39:39 +0100

    updated index

--- lib/views/index.rb
+++ lib/views/index.rb
@@ -6,10 +6,10 @@ class Index
     prompt = TTY::Prompt.new
     scrapper = Scrapping.new
 
-    prompt.select("Choose where you want to save your scrapp") do |menu|
-      menu.choice 'JSON', scrapper.save_as_json
-      menu.choice 'Spreadsheet', scrapper.save_as_spreadsheet
-      menu.choice 'CSV', scrapper.save_as_csv
-    end      
+    prompt.select("Choose where you want to save your scrapp", cycle: true) do |menu|
+      menu.choice 'JSON', -> { scrapper.save_as_json }
+      menu.choice 'Spreadsheet', -> { scrapper.save_as_spreadsheet }
+      menu.choice 'CSV', -> { scrapper.save_as_csv }
+    end
   end
 end
